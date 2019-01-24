ca11a65f541afe1b06f9e63399e9a98b1714ea14
Saevus96 <kp.chlewski@gmail.com>
2019-01-24 15:29:25 +0100

    skonczony neuron sigmoidalny

--- neuron sigmoidalny/main.cpp
+++ neuron sigmoidalny/main.cpp
@@ -1,101 +1,86 @@
-#include<iostream>
-#include<cmath>
+#include <iostream>
+#include <math.h>
 
-double beta = 4.0;
-double f(double s)
-{	
-	return(1.0 - exp(-beta * s)) / (1.0 + exp(-beta * s));
-}
-double fprim(double f) {
-	return beta * (1 - f * f);
+double beta = 0.6;
+
+double f(double s) {
+	return (1 - exp(-beta * s)) / (1 + exp(-beta * s));
 }
 
-int main() {
+double f1(double s) {
+	return beta * (1 - f(s)*f(s));
+}
 
-	//wartosci
-	double tabx1[4] = {1.0, -2.0, 0.0, -1.0};
-	double tabx2[4] = { 0, 1.5, -0.5,-1 };
-	double tabx3[4] = { -1, 1, 0.5, -1 };
+int main()
+{
+
+	double tabx1[5] = { 1,-2, 0,-1, 1 }; //x1
+	double tabx2[5] = { 0,1.5,-0.5,-1,1 }; //x2
+	double tabx3[5] = { -1,1,0.5,-1,1 }; //x3
+
+	//waga
+	double wx1[5] = { 0, 0, 0, 0, 0 };
+	double wx2[5] = { 0, 0, 0, 0, 0 };
+	double wx3[5] = { 0, 0, 0, 0, 0 };
 
 	//wartosci oczekiwane
-	double d1 = -1.0;
+	double d1 = -1;
 	double d2 = -1;
 	double d3 = 1;
 
-	//przypisanie wag 
-	double wx1[4] = { 0.0,0.0,0.0,0.0 };
-	double wx2[4] = { 0.0,0.0,0.0,0.0 };
-	double wx3[4] = { 0.0,0.0,0.0,0.0 };
-
-	//wpolczynnik uczenia
+	//wspó³czynnik uczenia
 	double ni = 0.4;
 
-	//blad 
+	double y1 = 0;
+	double y2 = 0;
+	double y3 = 0;
+	double suma1 = 0;
+	double suma2 = 0;
+	double suma3 = 0;
+
+	//b³ad
 	double E;
 	double E1;
 	double E2;
-	double E3;	
+	double E3;
 
-	//wartosc funkcji
-	double y1;
-	double y2;
-	double y3;
-
-	//wartosc fprim
-	double y1prim;
-	double y2prim;
-	double y3prim;
-
-	//suma
-	double sum;
-
-	//krok programu
-	int d = 0;
 	do {
+		E = 0;
 
-		//dla x1
-		sum = 0;
-		for (int i = 0; i < 4; i++) {
-			sum = sum + tabx1[i] * wx1[i];
-			y1 = f(sum);
-			y1prim = fprim(y1);
-			wx1[i] = wx1[i] + ni * (d1 - y1)*y1prim*tabx1[i];
-			E1 = (0.5)*((d1 - y1)*(d1 - y1));
-			d++;
+		for (int i = 0; i < 5; i++)
+		{
+			suma1 += tabx1[i] * wx1[i];
+			suma2 += tabx2[i] * wx2[i];
+			suma3 += tabx3[i] * wx3[i];
 		}
 
-		//dla x2
+		y1 = f(suma1);
+		y2 = f(suma2);
+		y3 = f(suma3);
 
-		sum = 0;
-		for (int i = 0; i < 4; i++) {
-			sum = sum + tabx2[i] * wx2[i];
-			y2 = f(sum);
-			y2prim = fprim(y2);
-			wx2[i] = wx2[i] + ni * (d2 - y2)*y2prim*tabx2[i];
-			E2 = (0.5)*((d2 - y2)*(d2 - y2));
-			d++;
+		for (int i = 0; i < 5; i++)
+		{
+			wx1[i] = wx1[i] + ni * (d1 - f(suma1)) * f1(y1) * tabx1[i];
+			wx2[i] = wx2[i] + ni * (d2 - f(suma2)) * f1(y2) * tabx2[i];
+			wx3[i] = wx3[i] + ni * (d3 - f(suma3)) * f1(y3) * tabx3[i];
 		}
 
-		//dla x3
+		E1 = 0.5 * (d1 - y1)*(d1 - y1);
+		E2 = 0.5 * (d2 - y2)*(d2 - y2);
+		E3 = 0.5 * (d3 - y3)*(d3 - y3);
 
-		sum = 0;
-		for (int i = 0; i < 4; i++) {
-			sum = sum + tabx3[i] * wx3[i];
-			y3 = f(sum);
-			y3prim = fprim(y3);
-			wx3[i] = wx3[i] + ni * (d3 - y3)*y3prim*tabx3[i];
-			E3 = (0.5)*((d3 - y3)*(d3 - y3));
-			d++;
-		}
-		d++;
-		E = E1 + E2 + E3;
-		
-		if (E < 0.03) {
-			std::cout << "Wynik dla x1: " << y1 << "  Wynik dla x2: " << y2 << "  Wynik dla x3: " << y3 << std::endl;
-			std::cout << "Kroki programu:  " << d<<std::endl;
-		}
-	} while (E > 0.03);
+		E = E1 + E3 + E2;
+
+
+
+	} while (E >= 0.0003);
+
+	std::cout << "wynik dla tabx1: " << y1 <<" Wynik dla tabx2: "<<y2<<"  Wynik dla tabx3: "<<y3<< std::endl;
 	
-	system("pause");
+
+
+
+
+	system("PAUSE");
 	return 0;
 }
